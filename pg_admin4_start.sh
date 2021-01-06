cc3a257a60e8031285ed3b80cda7b5a91db0112e
Slava <neo098765@gmail.com>
2021-01-06 23:49:58 +0300

    fix run pgadmin4

--- pg_admin4_start.sh
+++ pg_admin4_start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 docker pull dpage/pgadmin4
 docker run --rm --network host \
+    -v pgadmin4_root:/var/lib/pgadmin \
     -e 'PGADMIN_DEFAULT_EMAIL=user@domain.com' \
     -e 'PGADMIN_DEFAULT_PASSWORD=SuperSecret' \
     -e 'PGADMIN_LISTEN_PORT=5050' \
